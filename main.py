55d18ea6b7e9be2f5c03f6abca42b558cf6030f7
Dian <dian@gho.co.za>
2023-08-14 11:56:37 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from text_converter import TextConverter
+from text_to_audio_converter import TextToAudioConverter
+
+
+print("Hello")
---
+++ text_converter.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+class TextConverter:
+    pass
+
+
+
+
+
+
